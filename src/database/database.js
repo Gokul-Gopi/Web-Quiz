54f2e9cdc65fa6b3ca053d185106524ff748cb57
Gokul606 <68158600+Maxx665@users.noreply.github.com>
2021-09-15 01:39:33 +0530

    feat: new quiz topic

--- src/database/database.js
+++ src/database/database.js
@@ -120,5 +120,128 @@ export const dataBase = [
 
 
         ]
+    },
+
+    {
+        id: 2,
+        quizName: 'CSS Quiz',
+        questions: [
+            {
+                question: 'Which of the following selector matches all elements of a type?',
+                options: [
+                    {
+                        option: 'The Type Selector',
+                        isRight: true
+                    },
+                    {
+                        option: 'The Universal Selector',
+                        isRight: false
+                    },
+                    {
+                        option: 'The Descendant Selector',
+                        isRight: false
+                    },
+                    {
+                        option: 'The Class Selector',
+                        isRight: false
+                    },
+                ],
+                points: 5
+            },
+
+            {
+                question: 'Which of the following property is used to set the background color of an element?',
+                options: [
+                    {
+                        option: 'background-color',
+                        isRight: true
+                    },
+                    {
+                        option: 'background-image',
+                        isRight: false
+                    },
+                    {
+                        option: ' background-repeat',
+                        isRight: false
+                    },
+                    {
+                        option: 'background-position',
+                        isRight: false
+                    },
+                ],
+                points: 5
+            },
+
+            {
+                question: 'Which of the following property is used as shorthand to specify a number of other font properties?',
+                options: [
+                    {
+                        option: 'font-size',
+                        isRight: false
+                    },
+                    {
+                        option: 'font',
+                        isRight: true
+                    },
+                    {
+                        option: 'font-variant',
+                        isRight: false
+                    },
+                    {
+                        option: 'font-weight',
+                        isRight: false
+                    },
+                ],
+                points: 5
+            },
+
+            {
+                question: 'Which of the following value of cursor shows it as a pointing hand?',
+                options: [
+                    {
+                        option: 'crosshair',
+                        isRight: false
+                    },
+                    {
+                        option: 'default',
+                        isRight: false
+                    },
+                    {
+                        option: 'pointer',
+                        isRight: true
+                    },
+                    {
+                        option: 'move',
+                        isRight: false
+                    },
+                ],
+                points: 5
+            },
+
+            {
+                question: 'CSS stands for -',
+                options: [
+                    {
+                        option: 'Cascade style sheets',
+                        isRight: false
+                    },
+                    {
+                        option: 'Color and style sheets',
+                        isRight: false
+                    },
+                    {
+                        option: 'Cascading style sheets',
+                        isRight: true
+                    },
+                    {
+                        option: 'None of the above',
+                        isRight: false
+                    },
+                ],
+                points: 5
+            },
+
+
+        ]
     }
 ]
